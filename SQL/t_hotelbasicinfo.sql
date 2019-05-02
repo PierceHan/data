d84eebcf392cab840cb31231ca7fc0f5c76ad490
hga <han2770@163.com>
2019-05-02 15:08:48 +0800

    feat:更新酒店表结构

--- SQL/t_hotelbasicinfo.sql
+++ SQL/t_hotelbasicinfo.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50717
  File Encoding         : 65001
 
- Date: 01/05/2019 16:51:46
+ Date: 02/05/2019 15:08:13
 */
 
 SET NAMES utf8mb4;
@@ -28,14 +28,18 @@ CREATE TABLE `t_hotelbasicinfo`  (
   `cbd` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `star` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `rank` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `Person` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `Phone` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `Time` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP,
+  `pagedesc` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of t_hotelbasicinfo
 -- ----------------------------
-INSERT INTO `t_hotelbasicinfo` VALUES ('518e4f2d194c4e4f92a6ec4adf900319', 'wsc', '北京', '万达1', '4', '4');
-INSERT INTO `t_hotelbasicinfo` VALUES ('d2fa6e2f835c4244bb05d06375326c06', 'zjp001', '上海', '龙之梦001', '4', '3');
-INSERT INTO `t_hotelbasicinfo` VALUES ('uuidtest', '如家0000111', '上海', '龙之梦', '4', '5');
+INSERT INTO `t_hotelbasicinfo` VALUES ('518e4f2d194c4e4f92a6ec4adf900319', 'wsc', '北京', '万达1', '4', '4', NULL, NULL, '2019-05-02 14:46:25', NULL);
+INSERT INTO `t_hotelbasicinfo` VALUES ('d2fa6e2f835c4244bb05d06375326c06', 'zjp001', '上海', '龙之梦001', '4', '3', NULL, NULL, '2019-05-02 14:46:25', NULL);
+INSERT INTO `t_hotelbasicinfo` VALUES ('uuidtest', '如家0000111', '上海', '龙之梦', '4', '5', NULL, NULL, '2019-05-02 14:46:25', NULL);
 
 SET FOREIGN_KEY_CHECKS = 1;
